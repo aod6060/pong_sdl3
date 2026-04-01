5c8109ac03935ab0097452ea92bedb6a243d47af
aod6060 <a.o.d.6060@gmail.com>
2026-04-01 14:31:48 -0500

    #7 Added in render interface.

---
+++ src/engine/render.cpp
@@ -0,0 +1,31 @@
+#include "sys.hpp"
+
+
+namespace render {
+
+
+    void init() {
+        
+    }
+
+    void release() {
+        
+    }
+    
+    void clear(glm::vec4 clearColor) {
+        
+    }
+
+    void setProjection(glm::mat4 m) {
+        
+    }
+
+    void setModel(glm::mat4 m) {
+        
+    }
+
+    void draw() {
+        
+    }
+
+}
--- src/engine/sys.hpp
+++ src/engine/sys.hpp
@@ -446,6 +446,17 @@ namespace render {
         };
 
     }
+
+
+    void init();
+    void release();
+    
+    void clear(glm::vec4 clearColor);
+
+    void setProjection(glm::mat4 m);
+    void setModel(glm::mat4 m);
+
+    void draw();
 }
 
 #endif
